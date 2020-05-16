5058dc3952654cfc6c278a66512fcda073ac7048
ayabk <bkhateraya@gmail.com>
2020-05-16 04:58:37 +0100

    added Meditation page and Exercices page deleted some unused pictures

--- SleepAppPfa/lib/models/articlesClasses.dart
+++ SleepAppPfa/lib/models/articlesClasses.dart
@@ -23,19 +23,44 @@ List<Article> articlesHelpMe = [
         '2.A Handful of Nuts',
         '3.Cottage Cheese',
         '4.A Cup of Bedtime Tea',
-        '5.A Cup of Bedtime Tea',
+        '5.Warm Milk',
         '6.Fruits'
       ],
       paragraphes: [
-                "Skip the white bread, refined pasta, and sugary, baked goods, which may reduce serotonin levels and impair sleep. Instead, choose stick-to-your-ribs whole grains for your bedtime snack: Popcorn, oatmeal, or whole-wheat crackers with nut butter are all good choices.",
-                "Nuts are a good source of heart-healthy fats. And almonds and walnuts, specifically, contain melatonin, a hormone that helps to regulate your sleep/wake cycle. Eating them can increase your blood levels of the hormone, helping you sleep more soundly. ",
-                "Foods that are high in lean protein, like cottage cheese, also pack the amino acid tryptophan, which may increase serotonin levels. Serotonin is a brain chemical and low levels of it can contribute to insomnia. To sweeten it up, top the cottage cheese with raspberries, which are rich sources of melatonin.",
-                "A nightly cup of tea (sans caffeine, of course) can be a perfect relaxing ritual. Chamomile, ginger, and peppermint are calming choices for bedtime.",
-                "Scientifically, there may be some link between the tryptophan and melatonin content of milk and improved sleep. But perhaps more powerful is the psychological link between warm milk and bedtime as a child. Just like hot tea, a warm drink of milk can provide the perfect soothing backdrop for a relaxing bedtime routine.",
-                "Certain fruits that contain melatonin may help you fall asleep faster and wake up less often during the night. For instance, tart cherry juice and whole tart cherries contain a lot of melatonin, and bananas, pineapple, and oranges are also sources. If you have insomnia, eating two kiwis before bed can increase your sleep duration by an hour over the course of a month. Other fruits and vegetables that are rich in antioxidants (like berries, prunes, raisins, and plums) may have a similar effect by helping to counteract the oxidative stress caused by a sleep disorder."
-
-         ],
+        "Skip the white bread, refined pasta, and sugary, baked goods, which may reduce serotonin levels and impair sleep. Instead, choose stick-to-your-ribs whole grains for your bedtime snack: Popcorn, oatmeal, or whole-wheat crackers with nut butter are all good choices.",
+        "Nuts are a good source of heart-healthy fats. And almonds and walnuts, specifically, contain melatonin, a hormone that helps to regulate your sleep/wake cycle. Eating them can increase your blood levels of the hormone, helping you sleep more soundly. ",
+        "Foods that are high in lean protein, like cottage cheese, also pack the amino acid tryptophan, which may increase serotonin levels. Serotonin is a brain chemical and low levels of it can contribute to insomnia. To sweeten it up, top the cottage cheese with raspberries, which are rich sources of melatonin.",
+        "A nightly cup of tea (sans caffeine, of course) can be a perfect relaxing ritual. Chamomile, ginger, and peppermint are calming choices for bedtime.",
+        "Scientifically, there may be some link between the tryptophan and melatonin content of milk and improved sleep. But perhaps more powerful is the psychological link between warm milk and bedtime as a child. Just like hot tea, a warm drink of milk can provide the perfect soothing backdrop for a relaxing bedtime routine.",
+        "Certain fruits that contain melatonin may help you fall asleep faster and wake up less often during the night. For instance, tart cherry juice and whole tart cherries contain a lot of melatonin, and bananas, pineapple, and oranges are also sources. If you have insomnia, eating two kiwis before bed can increase your sleep duration by an hour over the course of a month. Other fruits and vegetables that are rich in antioxidants (like berries, prunes, raisins, and plums) may have a similar effect by helping to counteract the oxidative stress caused by a sleep disorder."
+      ],
       imageUrlA: "assets/images/Foods-that-Help-You-Sleep-Circle.png",
       titleA: "Grocery list for more restful nights."),
-  
+     
+  Article(
+      imageUrl: "",
+      miniTitles: [
+        '1. Bear hug',
+        '2. Neck stretches',
+        '3. Kneeling lat stretch ',
+        '4. Child’s pose ',
+        '5. Low lunge ',
+        '6. Seated forward bend',
+        '7. Legs-up-the-wall pose',
+        '8. Reclining bound angle pose ',
+      ],
+      paragraphes: [
+        "This stretch works the rhomboids and trapezius muscles of your upper back. It helps to alleviate shoulder blade discomfort or pain that’s caused by poor posture, bursitis, or frozen shoulder.\n\nTo do this stretch:\n\n1.Stand tall and inhale as you open your arms out wide.\n2.Exhale as you cross your arms, placing your right arm over your left and your left over your right to give yourself a hug.\n3.Breathe deeply as you use your hands to draw your shoulders forward.\n4.Hold this stretch for 30 seconds.\n5.To release, inhale to open your arms back open wide.\n6.Exhale and repeat with your left arm on top.",
+        "These stretches will help to relieve tension in your head, neck, and shoulders. Try to focus on maintaining good posture when doing these.\n\nTo do these stretches:\n\n1.Sit in a comfortable chair. Take your right hand to the top of your head or to your left ear.\n2.Gently bring your right ear towards your right shoulder, holding this position for 5 breaths.\n3.Repeat on the opposite side.\n4.Turn to look over your right shoulder, keeping the rest of your body facing forward.\n5.Hold this position for 5 breaths.\n6.Repeat on the opposite side.\n\n1.Drop your chin down to your chest, holding it here for 5 breaths.\n2.Return to a neutral position and allow your head to gently fall back for 5 breaths.",
+        "This stretch helps to loosen up the muscles in your back and shoulders, relieving pain and discomfort.\n\nTo do this stretch:\n\n1Come into a kneeling position in front of a chair, couch, or low table.\n2.Check that your knees are directly under your hips. You can rest on a blanket or cushion for extra support.\n3.Lengthen your spine as you hinge at the hips to fold forward, resting your forearms on the surface with your palms facing together.\n4.Hold this stretch for 30 seconds.\n5.Repeat 1 to 3 times.",
+        "Child’s pose is a resting stretch that’s similar to a kneeling lat stretch, but more relaxed. It’s perfect for tuning into your breath, relaxing your body, and reducing stress. It also helps to relieve pain and tension in your back, shoulders, and neck.\n\nTo do this:\n\n1.Come down on your knees, sitting back on your heels.\n2.Hinge at your hips to fold forward and rest your forehead on the floor.\n3.Extend your arms in front of you to support your neck or bring your arms alongside your body. You can use a pillow or cushion under your thighs or forehead for extra support.\n4.Breathe deeply in while holding the pose, bringing your awareness to any areas of discomfort or tightness in your back.\n5.Hold this pose for up to 5 minutes. You can also come into this pose between other stretches to give your body a rest.",
+        "This lunge stretches your hips, thighs, and groin. Opening your chest helps to relieve tension and pain in this area as well as your back and shoulders. Try to stay relaxed when doing this pose, and don’t push yourself too hard.\n\nTo do this stretch:\n\n1.Come into a low lunge with your right foot below your right knee and your left leg extended back, keeping your knee on the floor.\n2.Bring your hands to the floor beneath your shoulders, on your knees, or up toward the ceiling.\n3.Breathe deeply, focusing on lengthening your spine and opening your chest.\n4.Feel the line of energy extending out through the crown of your head.\n5.Hold this pose for 5 breaths.\n6.Repeat on the opposite side.",
+        "This stretch helps to loosen up your spine, shoulders, and hamstrings. It also stretches your lower back.\n\nTo do this stretch:\n\n1.Sit down with your legs extended in front of you.\n2.Engage your abdominal slightly to lengthen your spine, pressing your sit bones into the floor.\n3.Hinge at your hips to fold forward, reaching out your arms in front of you.\n4.Relax your head and tuck your chin into your chest.\n4.Hold this pose for up to 5 minutes.",
+        "This is a restorative pose that helps to reduce tension in your back, shoulders, and neck while promoting relaxation.\n\nTo do this stretch:\n\n1.Sit with the right side of your body against a wall.\n2.Lie on your back as you swing your legs up against the wall.\n3.Your hips can be up against the wall or a few inches away. Choose the distance that feels most comfortable. You can also place a cushion under your hips for support and a bit of elevation.\n4.Rest your arms in any comfortable position.\n5.Remain in this pose for up to 10 minutes.",
+        "This relaxing hip opener can help to relieve muscle tension in your hips and groin, making it especially good if you spend most of your day sitting.\n\nTo do this stretch:\n\n1.Sit on the floor and bring the soles of your feet together.\n2.Lean back on your hands to bring your back, neck, and head to the floor. You can use cushions or pillows under your knees or head for support.\n3.Place your arms in any comfortable positions.\n4.Focus on relaxing your hips and thighs as you breathe deeply.\n5.Hold this pose for up to 10 minutes.",
+      ],
+      imageUrlA: "assets/images/exercices.jpg",
+      titleA: "More Stretching Less Stressing."
+      ),
+
 ];
--- SleepAppPfa/lib/screens/meditation.dart
+++ SleepAppPfa/lib/screens/meditation.dart
@@ -1,8 +1,7 @@
-// import 'package:assets_audio_player/assets_audio_player.dart';
-// import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 import 'package:flutter/material.dart';
-// import 'package:audioplayers/audioplayers.dart';
 
 import '../widgets.dart';
 
@@ -12,123 +11,148 @@ class MeditationPage extends StatefulWidget {
 }
 
 class _MeditationState extends State<MeditationPage> {
-//   AssetsAudioPlayer _assetsAudioPlayer;
+  AudioPlayer _audioPlayer = AudioPlayer();
+  bool isPlaying = false;
+  bool firstTime = true;
 
-  
-// @override
-//   void initState() {
-//     super.initState();
-//     _assetsAudioPlayer = AssetsAudioPlayer();
-//     _assetsAudioPlayer.open(
-//       AssetsAudio(
-//         asset: "song3.mp3",
-//         folder: "assets/music/",
-//       ),
-//     );
-//     _assetsAudioPlayer.playOrPause();
-//   }
+  String currentTime = "00:00";
+  String completeTime = "00:00";
+  @override
+  void dispose() {
+    _audioPlayer.stop();
+    super.dispose();
+  }
 
-//   @override
-//   void dispose() {
-//     _assetsAudioPlayer = null;
-//     super.dispose();
-//   }
+  @override
+  void initState() {
+    super.initState();
+
+    _audioPlayer.onAudioPositionChanged.listen((Duration duration) {
+      setState(() {
+        currentTime = duration.toString().split(".")[0];
+      });
+    });
+
+    _audioPlayer.onDurationChanged.listen((Duration duration) {
+      setState(() {
+        completeTime = duration.toString().split(".")[0];
+      });
+    });
+  }
+
+  void startPlayingFromScratch() async {
+    String filePath = "audios/meditate.mp3";
+    final cache = AudioCache();
+
+    _audioPlayer = await cache.play(filePath);
+    debugPrint(filePath);
+    setState(() {
+      firstTime = true;
+      isPlaying = true;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
-    return SafeArea(
-        child: Scaffold(
-            appBar: AppBar(
-              leading: Container(),
-              title: Text("Meditation"),
-              centerTitle: true,
-            ),
-            bottomNavigationBar: customNavBar(context, 0),
-            body: SingleChildScrollView(
-                child: Container(
-              height: size.height * .81,
-              color: Colors.orange[100],
-              child: Stack(
-                children: <Widget>[
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.orange[100],
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/relaxation-png-1.png"),
-                        alignment: Alignment.topLeft,
-                      ),
-                    ),
-                  ),
-                  // Container(
-                  //   width: MediaQuery.of(context).size.width * 0.8,
-                  //   height: 80,
-                  //   margin: EdgeInsets.only(
-                  //       top: MediaQuery.of(context).size.height * 0.7,
-                  //       left: MediaQuery.of(context).size.width * 0.1),
-                  //   decoration: BoxDecoration(
-                  //       color: Colors.white,
-                  //       borderRadius: BorderRadius.circular(50)),
-                  //   child: Row(
-                  //     mainAxisAlignment: MainAxisAlignment.center,
-                  //     mainAxisSize: MainAxisSize.max,
-                  //     children: <Widget>[
-                  //       IconButton(
-                  //         icon:
-                  //             Icon(isPlaying ? Icons.pause : Icons.play_arrow),
-                  //         onPressed: () {
-                  //           if (isPlaying) {
-                  //             _audioPlayer.pause();
-
-                  //             setState(() {
-                  //               isPlaying = false;
-                  //             });
-                  //           } else {
-                  //             _audioPlayer.resume();
-
-                  //             setState(() {
-                  //               isPlaying = true;
-                  //             });
-                  //           }
-                  //         },
-                  //       ),
-                  //       SizedBox(
-                  //         width: 16,
-                  //       ),
-                  //       IconButton(
-                  //         icon: Icon(Icons.stop),
-                  //         onPressed: () {
-                  //           _audioPlayer.stop();
-
-                  //           setState(() {
-                  //             isPlaying = false;
-                  //           });
-                  //         },
-                  //       ),
-                  //       Text(
-                  //         currentTime,
-                  //         style: TextStyle(fontWeight: FontWeight.w700),
-                  //       ),
-                  //       Text(" | "),
-                  //       Text(
-                  //         completeTime,
-                  //         style: TextStyle(fontWeight: FontWeight.w300),
-                  //       ),
-                  //     ],
-                  //   ),
-                  // ),
-                  // // FloatingActionButton(
-                  //   child: Icon(Icons.audiotrack),
-                  //   onPressed: () {
-                  //     String filePath = "assets/audios/meditate.mp3";
-
-                  //     _audioPlayer.play(filePath);
-
-                  //     setState(() {
-                  //       isPlaying = true;
-                  //     });
-              //       },
-              //     ),
-                ],
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(),
+        title: Text("Meditate"),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: customNavBar(context, 0),
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.orange[50],
+                image: DecorationImage(
+                  image: AssetImage("assets/images/relaxation-png-1.png"),
+                ),
               ),
-            ))));
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  height: 80,
+                  margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.65,
+                    left: MediaQuery.of(context).size.width * 0.1,
+                    right: MediaQuery.of(context).size.width * 0.1,
+                  ),
+                  decoration: BoxDecoration(
+                      color: Colors.orange[100],
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.orange[100].withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(3,0,), // changes position of shadow
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(50)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+                        onPressed: () {
+                          if (isPlaying) {
+                            _audioPlayer.pause();
+
+                            setState(() {
+                              firstTime = false;
+                              isPlaying = false;
+                            });
+                          } else {
+                            if (!firstTime) {
+                              _audioPlayer.resume();
+
+                              setState(() {
+                                firstTime = false;
+                                isPlaying = true;
+                              });
+                            } else {
+                              startPlayingFromScratch();
+                            }
+                          }
+                        },
+                      ),
+                      SizedBox(
+                        width: 16,
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.stop),
+                        onPressed: () {
+                          _audioPlayer.stop();
+
+                          setState(() {
+                            isPlaying = false;
+                          });
+                        },
+                      ),
+                      Text(
+                        currentTime,
+                        style: TextStyle(fontWeight: FontWeight.w700),
+                      ),
+                      Text(" | "),
+                      Text(
+                        completeTime,
+                        style: TextStyle(fontWeight: FontWeight.w300),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      // ),
+    );
   }
 }
